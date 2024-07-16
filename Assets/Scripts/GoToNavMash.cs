66dd18934e478984c236a3246aee6bf800420eec
Viktordsa <viktorbobkov21@gmail.com>
2024-07-16 12:00:52 +0300

    + resp

---
+++ Assets/Scripts/GoToNavMash.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GoToNavMash : MonoBehaviour
+{
+    public Transform goal;
+    NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    void Update()
+    {
+        agent.destination = goal.position;
+
+    }
+}
